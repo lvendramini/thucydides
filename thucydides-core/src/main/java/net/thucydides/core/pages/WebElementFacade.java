050617c9b3354466f89bbecb9dce7c45dc2fb58f
Laura Vendramini <lauravendramini@laura-vendraminis-computer-5.local>
2011-07-12 23:54:41 -0400

    Add methods to enter text and select from a drop down (wait til object is rendered, clear / do what needs to be done (select or enter text)

--- thucydides-core/src/main/java/net/thucydides/core/pages/WebElementFacade.java
+++ thucydides-core/src/main/java/net/thucydides/core/pages/WebElementFacade.java
@@ -4,10 +4,8 @@ import com.thoughtworks.selenium.Selenium;
 import net.thucydides.core.ThucydidesSystemProperty;
 import net.thucydides.core.webdriver.Configuration;
 import net.thucydides.core.webdriver.WebDriverFacade;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverBackedSelenium;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
@@ -20,6 +18,7 @@ public class WebElementFacade {
 
     private final WebElement webElement;
     private final WebDriver driver;
+    private RenderedPageObjectView renderedView;
 
     public WebElementFacade(final WebDriver driver, final WebElement webElement) {
         this.driver = driver;
@@ -132,5 +131,98 @@ public class WebElementFacade {
                     "The text '%s' was not found in the web element", textValue);
             throw new AssertionError(errorMessage);
         }
+    }
+
+    private void enterValueOnClearField(String textValue) {
+        webElement.clear();
+        webElement.sendKeys(textValue);
+    }
+
+    /**
+     * Waits for the text element to be rendered, clear the element, and enters the value followed by enter.
+     * @param textValue
+     */
+    public void enterValueWithEnter(String textValue) {
+        waitForElementToBeVisible();
+        enterValueOnClearField(textValue);
+        webElement.sendKeys(Keys.ENTER);
+    }
+
+
+    /**
+     * Waits for the text element to be rendered, clear the element, and enters the value followed by tab.
+     * @param textValue
+     */
+    public void enterValueWithTab(String textValue) {
+       //todo need a waitForRenderedElements but by the webElement (not by By.id)
+        enterValueOnClearField(textValue);
+        webElement.sendKeys(Keys.TAB);
+    }
+
+    /**
+     * Waits for the drop down to be rendered and selects the element based off the text displayed
+     * @param textValue
+     */
+    public void selectValueWithText(String textValue) {
+        waitForElementToBeVisible();
+        try {
+            Select select = new Select(webElement);
+            select.selectByVisibleText(textValue);
+        }
+        catch(Exception ex) {
+            String errorMessage = String.format(
+                    "The text '%s' was not found in the selectable element", textValue);
+            throw new AssertionError(errorMessage);
+        }
+
+    }
+
+    /**
+     * Waits for the drop down to be rendered and selects the element based off the value
+     * @param value
+     */
+    public void selectValueWithValue (String value) {
+        waitForElementToBeVisible();
+        try {
+            Select select = new Select(webElement);
+            select.selectByValue(value);
+        }
+        catch(Exception ex) {
+            String errorMessage = String.format(
+                    "The value '%s' was not found in the selectable element", value);
+            throw new AssertionError(errorMessage);
+        }
+    }
+
+    /**
+    * Waits for the drop down to be rendered and selects the element based off the index
+    * @param index
+    */
+    public void selectValueWithIndex(Integer index) {
+        waitForElementToBeVisible();
+        try {
+            Select select = new Select(webElement);
+            select.selectByIndex(index);
+        }
+        catch(Exception ex) {
+            String errorMessage = String.format(
+                    "The index '%s' was not found in the selectable element", index);
+            throw new AssertionError(errorMessage);
+        }
+
+    }
+
+
+    protected RenderedPageObjectView getRenderedView() {
+        long waitForTimeout = 5;
+        if (renderedView == null) {
+            renderedView = new RenderedPageObjectView(driver, waitForTimeout);
+        }
+        return renderedView;
+    }
+
+    private void waitForElementToBeVisible()
+    {
+        getRenderedView().waitForRenderedElement(webElement);
     }
 }
